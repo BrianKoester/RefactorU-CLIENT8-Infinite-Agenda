7dd3b1857c7efd2a0e7e76b0add9c89f1a0db54c
RU-014 <RU-014@Admins-Mac-mini-76.local>
2013-10-05 16:52:22 -0600

    first commit - incomplete

---
+++ agenda.js
@@ -0,0 +1,74 @@
+// just copy this function into your utility.js
+// don't try to understand this until you have jedi powers
+if (!String.prototype.supplant) {
+    String.prototype.supplant = function (o) {
+        return this.replace(
+            /\{([^{}]*)\}/g,
+            function (a, b) {
+                var r = o[b];
+                return typeof r === 'string' || typeof r === 'number' ? r : a;
+            }
+        );
+    };
+}
+
+$(document).ready(function(){
+
+	// Initialize variables and arrays
+	var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+	var weekday = 0;
+	var Today = new Date()
+	var weekday = Today.getUTCDay()
+	console.log("day", weekday);
+
+
+	// Select and edit appointment fields
+	function divClicked() {
+	    var divHtml = $(this).html();
+	    var editableText = $("<textarea class='col-xs-9' />");
+	    editableText.val(divHtml);
+	    $(this).replaceWith(editableText);
+	    editableText.focus();
+	    // setup the blur event for this new textarea
+	    editableText.blur(editableTextBlurred);
+	}
+
+	function editableTextBlurred() {
+	    var html = $(this).val();
+	    var viewableText = $("<div class='col-xs-9 appointment' />");
+	    viewableText.html(html);
+	    $(this).replaceWith(viewableText);
+	    // setup the click event for this new div
+	    viewableText.click(divClicked);
+	}
+	
+		// Enable appointment editing
+	   	$(".appointment").click(divClicked);
+
+	    // Detect that we are at bottom of page, and call autoloading function
+        $(window).scroll(function() { 
+            if  ($(window).scrollTop()+250 >= ($(document).height() - ($(window).height()))){
+
+                    // Add another day
+                    for (i=0; i<days.length; i++) {
+                    	var incrementDay = days[i];
+
+	                    var newDay = ('<div class="row">' + 
+		  								'<div class="col-xs-3">' + incrementDay + '</div>' +
+		  								'<div class="col-xs-9 appointment">Appointment</div>' +
+										'</div>');
+						
+	                    $('.container').append(newDay);
+
+			       
+			        	// Select and edit appointment fields
+			        	$(".appointment").click(divClicked);
+
+		        	}
+            }
+
+		});
+		
+
+});
